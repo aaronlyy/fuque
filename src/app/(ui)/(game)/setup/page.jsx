f122e4a729d4947000bada489838d1d3c1f8f873
aaronlyy <aaronlevican@gmail.com>
2023-07-21 23:53:17 +0200

    ✨ Revalidate test

--- src/app/(ui)/(game)/setup/page.jsx
+++ src/app/(ui)/(game)/setup/page.jsx
@@ -1,27 +1,21 @@
 import { prisma } from "@/prisma"
 import Link from "next/link"
 
+export const revalidate = 60;
+
+async function getPacks() {
+  const packs = await prisma.pack.findMany();
+  return packs.map((pack) => <li key={pack.id}>{pack.name}</li>);
+}
+
 export default async function PageSetup() {
 
-  function onClickPack(pid) {
-    // check if pack is currently active
-    // make active if not
-    
-  }
-
-  // load all available packs with ids
-  const packs = await prisma.pack.findMany();
-  const packItems = packs.map((pack) => <li key={pack.id}>{pack.name}</li>);
-  // display all packs
-  // save clicked packs in localstorage
-  // when played load all packs?
-
-
+  const packs = await getPacks();
 
   return (
     <div className="PageSetup">
       <p>Choose packs</p>
-      <ul>{packItems}</ul>
+      <ul>{packs}</ul>
       <Link href='/play'>Play</Link>
     </div>
   )
