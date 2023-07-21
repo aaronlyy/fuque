48f873927d6092c62b82caadf9337e4e4ff229a7
aaronlyy <aaronlevican@gmail.com>
2023-07-21 20:41:39 +0200

    ❌ Removed auth

--- src/app/(ui)/layout.jsx
+++ src/app/(ui)/layout.jsx
@@ -1,12 +1,8 @@
-import NextAuthSessionProvider from "./components/provider/NextAuthSessionProvider"
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <NextAuthSessionProvider>
           {children}
-        </NextAuthSessionProvider>
       </body>
     </html>
   )
--- src/app/(ui)/page.jsx
+++ src/app/(ui)/page.jsx
@@ -1,16 +1,5 @@
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation"
-
 export default async function Home() {
-
-  const session = await getServerSession(authOptions)
-
-  if (!session) {
-    redirect('/api/auth/signin');
-  }
-
   return (
-    <h1>This is a protected page</h1>
+    <h1>Work in progress</h1>
   )
 }
