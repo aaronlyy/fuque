5abc18ca930358aeb4b85dd757f66cc485f9f432
aaronlyy <aaronlevican@gmail.com>
2023-07-21 15:15:12 +0200

    🔨 Fixed question adding route

--- src/app/api/question/route.js
+++ src/app/api/question/route.js
@@ -9,12 +9,12 @@ export async function POST(req) {
   // get request body
   const json = await req.json();
   // check if body is valid (question, language, pid, cid)
-  if (!json.question | !json.language | !json.pid | !json.cid) {
+  if (!json.question | !json.pid) {
     const data = {message: "Request JSON is missing some stuff"};
     return NextResponse.json(data, {status: 400});
   }
-  
-  // if body is valid, add needed information to json (created_at, modified_at, uid)
+
+  // if body is valid, add needed information to json (created_at, modified_at)
   const date = new Date();
 
   // save question to database
@@ -22,10 +22,7 @@ export async function POST(req) {
     await prisma.question.create({
       data: {
         question: json.question,
-        qid: 0,
-        cid: json.cid,
         pid: json.pid,
-        uid: 0,
         created_at: date,
         modified_at: date
       }
